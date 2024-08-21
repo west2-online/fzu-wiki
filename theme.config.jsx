b13a820f2d27ee66367b761ddcbca59f37e7be51
Baoshuo <i@baoshuo.ren>
2024-08-21 21:23:16 +0800

    chore: update footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -15,6 +15,25 @@ const config = {
       };
     }
   },
+  footer: {
+    text: (
+      <div style={{ width: '100%' }}>
+        Built with ❤️ by FZUers.
+        <br />
+        &copy; {new Date().getFullYear()} west2-online. All rights reserved.
+        <small style={{ float: 'right' }}>
+          Powered by{' '}
+          <a
+            href="https://nextra.site"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            Nextra
+          </a>
+        </small>
+      </div>
+    ),
+  },
 };
 
 export default config;
